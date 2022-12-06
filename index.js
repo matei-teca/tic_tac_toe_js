9450d1a313c28a51a5fbf5d92205310e9b2991ff
Valentin1289 <valibaiacof1289@gmail.com>
2022-12-06 22:53:34 +0200

    remake unbeateable and solve the bug

--- index.js
+++ index.js
@@ -42,6 +42,7 @@ function setGameMode(selectedValue) {
 // key in the input box labeled `enter coordinates`
 // paramerter: input - the content of the input box
 function processHumanCoordinate(input) {
+  let valid=true;
     console.log(`'processHumanCoordinate('${input}')`);
     if (gameTurn % 2 === 0) {
         currentPlayer = "diamond";
@@ -53,12 +54,19 @@ function processHumanCoordinate(input) {
 
     if(coordinates.x < 0 || coordinates.x > 2 || coordinates.y < 0 || coordinates.y > 2){
       displayMessage("Invalid coordinate entered");
+      valid=false;
     }
     else if(board[coordinates.x][coordinates.y] !== ""){
       displayMessage("Position is already taken on board");
+      valid=false;
     } 
     else {
       handleMove(coordinates, currentPlayer)
+    }
+    if(!valid){
+      if (e.key === 'Enter') {
+    return processHumanCoordinate(e.target.value);
+    }
     }
     if(!isPlayerYHuman){
       setHTMLvisibilityForInputHumanCoordinates(false);
@@ -87,15 +95,7 @@ function processAICoordinate() {
     else{
       coordinates= basicAI()
     }
-    if(coordinates.x < 0 || coordinates.x > 2 || coordinates.y < 0 || coordinates.y > 2){
-      displayMessage("Invalid coordinate entered");
-    }
-    else if(board[coordinates.x][coordinates.y] !== ""){
-      displayMessage("Position is already taken on board");
-    } 
-    else {
       handleMove(coordinates, currentPlayer)
-    }
   }
   
 if(Aimode==0){
@@ -124,71 +124,52 @@ checkThese1.push([[0,0], [1,1], [2,2]])
 checkThese1.push([[0,2], [1,1], [2,0]])
 
 function basicAI(){
-
+let X,O;
+if(gameTurn%2==1) {
+  X="pets"; O="diamond"
+}else {
+  X="diamond"; O="pets"
+}
   // if AI can win
-if(gameTurn%2==0)
-{for(line of checkThese1){
-  if(board[line[0][0]][line[0][1]]==board[line[1][0]][line[1][1]] && board[line[1][0]][line[1][1]]=="pets" && board[line[2][0]][line[2][1]]==""){
+  for(line of checkThese1){
+  if(board[line[0][0]][line[0][1]]==board[line[1][0]][line[1][1]] && board[line[1][0]][line[1][1]]==X && board[line[2][0]][line[2][1]]==""){
     return {x:line[2][0],y:line[2][1]}
-  }else if(board[line[0][0]][line[0][1]]==board[line[2][0]][line[2][1]] && board[line[2][0]][line[2][1]]=="pets" && board[line[1][0]][line[1][1]]==""){
+  }else if(board[line[0][0]][line[0][1]]==board[line[2][0]][line[2][1]] && board[line[2][0]][line[2][1]]==X && board[line[1][0]][line[1][1]]==""){
     return {x:line[1][0],y:line[1][1]}
-  }else if(board[line[1][0]][line[0][1]]==board[line[2][0]][line[2][1]] && board[line[2][0]][line[2][1]]=="pets" && board[line[0][0]][line[0][1]]==""){
+  }else if(board[line[1][0]][line[1][1]]==board[line[2][0]][line[2][1]] && board[line[2][0]][line[2][1]]==X && board[line[0][0]][line[0][1]]==""){
     return {x:line[0][0],y:line[0][1]}
   }
 
 }
-//if human is about to win
+//if enemy is about to win
 for(line of checkThese1){
-  if(board[line[0][0]][line[0][1]]==board[line[1][0]][line[1][1]] && board[line[1][0]][line[1][1]]=="diamond" && board[line[2][0]][line[2][1]]==""){
+  if(board[line[0][0]][line[0][1]]==board[line[1][0]][line[1][1]] && board[line[1][0]][line[1][1]]==O && board[line[2][0]][line[2][1]]==""){
     return {x:line[2][0],y:line[2][1]}
-  }else if(board[line[0][0]][line[0][1]]==board[line[2][0]][line[2][1]] && board[line[2][0]][line[2][1]]=="diamond" && board[line[1][0]][line[1][1]]==""){
+  }else if(board[line[0][0]][line[0][1]]==board[line[2][0]][line[2][1]] && board[line[2][0]][line[2][1]]==O && board[line[1][0]][line[1][1]]==""){
     return {x:line[1][0],y:line[1][1]}
-  }else if(board[line[1][0]][line[0][1]]==board[line[2][0]][line[2][1]] && board[line[2][0]][line[2][1]]=="diamond" && board[line[0][0]][line[0][1]]==""){
+  }else if(board[line[1][0]][line[1][1]]==board[line[2][0]][line[2][1]] && board[line[2][0]][line[2][1]]==O && board[line[0][0]][line[0][1]]==""){
+    console.log(gameTurn)
     return {x:line[0][0],y:line[0][1]}
-  }
-}
-}
-else{
-  for(line of checkThese1){
-    if(board[line[0][0]][line[0][1]]==board[line[1][0]][line[1][1]] && board[line[1][0]][line[1][1]]=="diamond" && board[line[2][0]][line[2][1]]==""){
-      return {x:line[2][0],y:line[2][1]}
-    }else if(board[line[0][0]][line[0][1]]==board[line[2][0]][line[2][1]] && board[line[2][0]][line[2][1]]=="diamond" && board[line[1][0]][line[1][1]]==""){
-      return {x:line[1][0],y:line[1][1]}
-    }else if(board[line[1][0]][line[0][1]]==board[line[2][0]][line[2][1]] && board[line[2][0]][line[2][1]]=="diamond" && board[line[0][0]][line[0][1]]==""){
-      return {x:line[0][0],y:line[0][1]}
-    }
-  }
-  for(line of checkThese1){
-    if(board[line[0][0]][line[0][1]]==board[line[1][0]][line[1][1]] && board[line[1][0]][line[1][1]]=="pets" && board[line[2][0]][line[2][1]]==""){
-      return {x:line[2][0],y:line[2][1]}
-    }else if(board[line[0][0]][line[0][1]]==board[line[2][0]][line[2][1]] && board[line[2][0]][line[2][1]]=="pets" && board[line[1][0]][line[1][1]]==""){
-      return {x:line[1][0],y:line[1][1]}
-    }else if(board[line[1][0]][line[0][1]]==board[line[2][0]][line[2][1]] && board[line[2][0]][line[2][1]]=="pets" && board[line[0][0]][line[0][1]]==""){
-      return {x:line[0][0],y:line[0][1]}
-    }
-  
   }
 }
 // prioritize the centre
 if(board[1][1]==""){ 
   return{x:1,y:1}
 }
-
-if(gameTurn==3 && board[1][1]=="pets"){
-if(board[0][1]=="")  return{x:0,y:1}
-else if(board[1][0]=="")  return{x:1,y:0}
-else if(board[2][1]=="")  return{x:2,y:1}
-else if(board[1][2]=="")  return{x:1,y:2}
+// if the enemy tryes tricks
+if(gameTurn==3 && board[1][1]==X){
+if(board[0][1]=="" && board[2][1]=="")  return{x:0,y:1}
+else if(board[1][0]=="" && board[1][2]=="")  return{x:1,y:0}
 }
 
 // if centre is taken AI goes for corners
-if(board[0][0]=="")
+if(board[0][0]=="" && board[0][1]==board[1][0] && board[0][1]!="")
 return{x:0,y:0}
-if(board[0][2]=="")
+if(board[0][2]=="" && board[0][1]==board[1][2] && board[0][1]!="")
 return{x:0,y:2}
-if(board[2][0]=="")
+if(board[2][0]=="" && board[2][1]==board[1][0] && board[2][1]!="")
 return{x:2,y:0}
-if(board[2][2]=="")
+if(board[2][2]=="" && board[2][1]==board[1][2] && board[2][1]!="")
 return{x:2,y:2}
 // Otherwise goes for the left empty tile
 for (let i = 0; i < 3; i++)
